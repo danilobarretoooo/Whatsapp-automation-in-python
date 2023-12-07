5b50cad692dd9216d73c838e2b94e2fb67dc221e
Danilo Barreto <120517434+danilobarretoooo@users.noreply.github.com>
2023-12-07 00:53:16 -0300

    Basic WhatsApp Automation Implementation

---
+++ app.py
@@ -0,0 +1,40 @@
+
+import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui
+import os 
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
+
+# Ler planilha e guardar informações sobre nome, telefone e data de vencimento
+workbook = openpyxl.load_workbook('clientes.xlsx')
+pagina_clientes = workbook['Sheet1']
+
+for linha in pagina_clientes.iter_rows(min_row=2):
+    # nome, telefone, vencimento
+    nome = linha[0].value
+    telefone = linha[1].value
+    vencimento = linha[2].value
+    
+    mensagem = f'Olá {nome} seu boleto vence no dia {vencimento.strftime('%d/%m/%Y')}. Favor pagar no link https://www.link_do_pagamento.com'
+
+    # Criar links personalizados do whatsapp e enviar mensagens para cada cliente
+    # com base nos dados da planilha
+    try:
+        link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+        webbrowser.open(link_mensagem_whatsapp)
+        sleep(10)
+        seta = pyautogui.locateCenterOnScreen('seta.png')
+        sleep(2)
+        pyautogui.click(seta[0],seta[1])
+        sleep(2)
+        pyautogui.hotkey('ctrl','w')
+        sleep(2)
+    except:
+        print(f'Não foi possível enviar mensagem para {nome}')
+        with open('erros.csv','a',newline='',encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome},{telefone}{os.linesep}')
+    
